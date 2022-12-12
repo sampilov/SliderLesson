380ff11b45b436d74ae8ccdb686ace8668e8ae53
Ildar Sampilov <ildar.sampilov@yandex.ru>
2022-12-12 13:53:31 +0600

    Initial Commit

---
+++ Lesson4.30/Lesson4_30App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4_30App.swift
+//  Lesson4.30
+//
+//  Created by Цырендылыкова Эржена on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4_30App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
